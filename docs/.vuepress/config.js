c0a918f385b9e90ce4d6facc086d9afd63b3a6a8
lml <soalin228@163.com>
2022-09-19 21:51:49 +0800

    feat: 初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+import { defineUserConfig,defaultTheme } from "vuepress";
+
+export default defineUserConfig({
+  lang: "zh-CN",
+  title: "循此苦旅，以达星辰",
+  description: "记录个人学习成长",
+  theme: defaultTheme({
+    navbar: [
+      {
+        text: "首页",
+        link: "/",
+      },
+      {
+        text: "Github",
+        link: "https://github.com/SOALIN228"
+      },
+    ],
+  }),
+});
